2c41da0591a17b05888d2afec5637dedc0950911
SparrowHe <sparrowhe@gmail.com>
2022-02-12 14:16:23 +0800

    added marks

--- static/js/oscar-map-v2.core.js
+++ static/js/oscar-map-v2.core.js
@@ -10,6 +10,17 @@ function init() {
         id: 'your.mapbox.project.id',
         accessToken: 'your.mapbox.public.access.token'
     }).addTo(map);
+    updMap();
+    //setInterval(updMap, 1000);
+}
+
+function checkDumpCallsign(callsign) {
+    for (let j = 0; j < player.length; j++) {
+        if (player[j].callsign == callsign) {
+            return j;
+        }
+    }
+    return -1;
 }
 
 function updMap() {
@@ -18,6 +29,22 @@ function updMap() {
         success: function (data) {
             let n = data.split("\n");
             n.pop();
+            for (let i = 0; i < player.length; i++) {
+                let d = player[i];
+                let flag = false;
+                for (let j = 0; j < n.length; j++) {
+                    let k = n[j].split(":");
+                    if (k[2] == d.callsign) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag) {
+                    map.removeLayer(d.marker);
+                    player.splice(i, 1);
+                    i--;
+                }
+            }
             for (let i = 0; i < n.length; i++) {
                 let t = n[i].split(":");
                 // console.log(t);
@@ -37,9 +64,10 @@ function updMap() {
                 d.route = t[11];
                 d.radarRange = parseFloat(t[12]);
                 d.form = t[13];
-                d.ydj = t[14];
-                d.type = t[15];
-                player.push(d);
+                d.squawk = t[14];
+                d.actype = t[15];
+                checkDumpCallsign(d.callsign) == -1 ? d.marker = null : d.marker = player[checkDumpCallsign(d.callsign)].marker;
+                checkDumpCallsign(d.callsign) == -1 ? player.push(d) : player[checkDumpCallsign(d.callsign)] = d;
                 // console.log(d);
             }
             addMark();
@@ -50,7 +78,45 @@ function updMap() {
 function addMark() {
     for (let i = 0; i < player.length; i++) {
         let d = player[i];
-        let marker = L.marker([d.lat, d.lng]).addTo(map);
-        marker.bindPopup(d.callsign);
+        // console.log(d);
+        // console.log(d.marker);
+        if (d.marker == null) {
+            if (d.type == "ATC") {
+                // set icon to ATC and add marker
+                let icon = L.icon({
+                    iconUrl: 'static/image/headset_mic.png',
+                    iconSize: [20, 20],
+                    iconAnchor: [10, 10],
+                    popupAnchor: [0, -15]
+                });
+                let marker = L.marker([d.lat, d.lng], {
+                    icon: icon
+                }).addTo(map);
+                // set pop up
+                marker.bindPopup(`<b>${d.callsign}</b><br>频率：${d.freq}<br>管制员：${d.id}`);
+                player[i].marker = marker;
+            } else if (d.type == "PILOT") {
+                let icon = L.icon({
+                    iconUrl: 'static/image/airplane.png',
+                    iconSize: [20, 20],
+                    iconAnchor: [10, 10],
+                    popupAnchor: [0, -15]
+                });
+                let marker = L.marker([d.lat, d.lng], {
+                    icon: icon,
+                    rotationAngle: d.heading
+                }).addTo(map);
+                // set pop up
+                marker.bindPopup(`<b>${d.callsign}</b><br>起飞/降落：${d.dep}/${d.arr}<br>高度：${d.alt}<br>航向：${d.heading}<br>航路：${d.route}<br>飞行员：${d.id}<br>机型：${d.actype}<br>应答机：${d.squawk}`);
+                player[i].marker = marker;
+            }
+        } else {
+            d.marker.setLatLng([d.lat, d.lng]);
+            if (d.type == "ATC") {
+                d.marker.bindPopup(`<b>${d.callsign}</b><br>频率：${d.freq}<br>管制员：${d.id}`);
+            } else if (d.type == "PILOT") {
+                d.marker.bindPopup(`<b>${d.callsign}</b><br>起飞/降落：${d.dep}/${d.arr}<br>高度：${d.alt}<br>航向：${d.heading}<br>航路：${d.route}<br>飞行员：${d.id}<br>机型：${d.actype}<br>应答机：${d.squawk}`);
+            }
+        }
     }
 }
